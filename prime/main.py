46f24322c5e27989801c699956c0dd03a0606d94
AlirezA Az <bojbaj@gmail.com>
2020-04-15 16:45:13 +0430

    prime algorithm optimized

--- prime/main.py
+++ prime/main.py
@@ -1,13 +1,22 @@
 import sys
+import math
 
-def is_prime(number: int) -> bool:
+def is_prime(number):
    if(number < 2):
       return False
-   for i in range(2, number):
+
+   if(number == 2):
+      return True
+
+   if(number % 2 == 0):
+      return False
+
+   sqrt_of_number = math.floor(math.sqrt(number)) 
+   for i in range(3, sqrt_of_number + 1, 2):
       if (number % i == 0):
          return False
    return True
 
 with open(sys.argv[1]) as inputs:
-   for input_data in inputs:   
+   for input_data in inputs:         
       print(1 if is_prime(int(input_data)) else 0)

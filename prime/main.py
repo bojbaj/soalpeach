11c9425891173b4cc94f0aef3e4b007bc95d4c87
AlirezA Az <bojbaj@gmail.com>
2020-04-16 10:59:41 +0430

    less number to check

--- prime/main.py
+++ prime/main.py
@@ -1,4 +1,6 @@
 import sys, math
+# import time
+# start_time = time.time()
 
 def is_prime(number: int) -> bool:
    if(number <= 3):
@@ -7,14 +9,16 @@ def is_prime(number: int) -> bool:
    if(number % 2 == 0 or number % 3 == 0):
       return False
 
-   sqrt_of_number = int(math.sqrt(number))
+   sqrt_of_number = min(int(math.sqrt(number)), 100)
    for i in range(5, sqrt_of_number, 6):
       if ((number % i == 0) or (number % (i+2) == 0)):
          return False
-   else:
+   else:      
       return True
 
 file_path = sys.argv[1]
 with open(file_path) as inputs:
-   for input_data in inputs:      
+   for input_data in inputs:   
       print(1 if is_prime(int(input_data)) else 0)
+
+# print("--- %s seconds ---" % (time.time() - start_time))

71f953b52bd3521fceee7a70124f8eb4412be96a
AlirezA Az <bojbaj@gmail.com>
2020-04-29 19:52:48 +0430

    remove response from post method

--- countme/CountMe/Startup.cs
+++ countme/CountMe/Startup.cs
@@ -1,13 +1,10 @@
 using System;
-using System.Net;
-using System.Net.Http;
 using CountMe.Helpers;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 
 namespace CountMe
 {
@@ -27,26 +24,22 @@ namespace CountMe
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
             app.UseRouting();
             Db db = new Db();
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapPost("/", async context =>
-               {
-                   int input = Convert.ToInt32(new System.IO.StreamReader(context.Request.Body).ReadToEnd());
-                   await context.Response.WriteAsync(db.SetNewNumber(input).ToString());
-               });
+                {
+                    int input = Convert.ToInt32(new System.IO.StreamReader(context.Request.Body).ReadToEnd());
+                    db.SetNewNumber(input);
+                    await context.Response.WriteAsync("");
+                    //    await context.Response.WriteAsync();
+                });
                 endpoints.MapGet("/count", async context =>
                 {
                     await context.Response.WriteAsync(db.GetSumOfNumbers().ToString());
                 });
             });
-            db.SetNewNumber(0);
         }
     }
 }

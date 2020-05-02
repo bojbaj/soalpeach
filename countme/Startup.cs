e07bf80e567a9af8743124bfbaf18541e4ecf933
Azarmanesh <Azarmanesh@AZARMANESH>
2020-05-02 10:52:27 +0330

    remove extra instance creation

--- countme/Startup.cs
+++ countme/Startup.cs
@@ -25,19 +25,21 @@ namespace CountMe
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             int SumOfNumbers = 0;
-            Db db = new Db();
-            System.IO.StreamReader sr = null;
+            // Db db = new Db();             
             app.UseRouting();
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapPost("/", async context =>
                 {
-                    sr = new System.IO.StreamReader(context.Request.Body);
-                    string strInput = await (sr.ReadToEndAsync());
-                    // int input = Convert.ToInt32(strInput);                    
-                    // await db.SetNewNumber(input);
-                    SumOfNumbers += Convert.ToInt32(strInput);
+                    using(System.IO.StreamReader sr = new System.IO.StreamReader(context.Request.Body))
+                    {
+                        string strInput = await (sr.ReadToEndAsync());                        
+                        // int input = Convert.ToInt32(strInput);                    
+                        // await db.SetNewNumber(input);
+                        SumOfNumbers += Convert.ToInt32(strInput);
+                    }
                     await context.Response.CompleteAsync();
+                    
                 });
                 endpoints.MapGet("/count", async context =>
                 {

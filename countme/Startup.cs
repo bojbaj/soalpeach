d09fd098a996bad22014f84fc020345c2634dbbc
Azarmanesh <Azarmanesh@AZARMANESH>
2020-05-02 12:06:53 +0330

    helper class rmeoved

--- countme/Startup.cs
+++ countme/Startup.cs
@@ -24,28 +24,30 @@ namespace CountMe
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            int proccessing = 0;
             int SumOfNumbers = 0;
-            // Db db = new Db();             
             app.UseRouting();
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapPost("/", async context =>
                 {
-                    using(System.IO.StreamReader sr = new System.IO.StreamReader(context.Request.Body))
+                    using (System.IO.StreamReader sr = new System.IO.StreamReader(context.Request.Body))
                     {
-                        string strInput = await (sr.ReadToEndAsync());                        
+                        proccessing++;
+                        string strInput = await (sr.ReadToEndAsync());
                         // int input = Convert.ToInt32(strInput);                    
                         // await db.SetNewNumber(input);
                         SumOfNumbers += Convert.ToInt32(strInput);
+                        proccessing--;
                     }
-                    await context.Response.CompleteAsync();
-                    
+                    // await context.Response.CompleteAsync();                    
                 });
                 endpoints.MapGet("/count", async context =>
                 {
+                    while (proccessing > 0) { }
                     // string result = (await db.GetSumOfNumbers()).ToString();
                     await context.Response.WriteAsync(SumOfNumbers.ToString());
-                    await context.Response.CompleteAsync();
+                    // await context.Response.CompleteAsync();
                 });
             });
         }
